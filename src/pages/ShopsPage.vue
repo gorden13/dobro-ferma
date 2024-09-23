2585b00e4fb91a8026d91dbbdd439c96ee82e0a3
Горынцев Денис <dl.goryntsev@samolet.ru>
2024-09-23 22:11:58 +0300

    DOBROFERMA/refactor: перевод на vue 3

--- src/pages/ShopsPage.vue
+++ src/pages/ShopsPage.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-  import UIWaterMark from '@/shared/ui/UIWaterMark.vue';
+  import UIWaterMark from '@/shared/ui/UiWaterMark.vue';
 </script>
 
 <style lang="scss" scoped>
