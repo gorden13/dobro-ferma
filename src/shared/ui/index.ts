c1f64e7fdf25d0187681bf239aabec5edbaf6bac
Горынцев Денис <dl.goryntsev@samolet.ru>
2024-09-23 22:11:33 +0300

    DOBROFERMA/refactor: перевод на vue 3

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,7 @@
-export { default as WaterMark } from './UIWaterMark.vue';
+export { default as UiWaterMark } from './UiWaterMark.vue';
 export { default as UiImage } from './UiImage.vue';
 export { default as UiPageSubHeader } from './UiPageSubHeader.vue';
-export { default as UiModalCard } from './UIModalCard.vue';
+export { default as UiModalCard } from './UiModalCard.vue';
 export { default as UiCurrencyInput } from './UiCurrencyInput.vue';
 export { default as UiUpload } from './UiUpload.vue';
 export { default as UiSimpleDialog } from './UiSimpleDialog.vue';
